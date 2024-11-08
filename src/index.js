5e6111fcfa95229116b4d519c0bf754664154b6d
Rémy <remy.arnaudin@sauermanngroup.com>
2024-11-08 11:55:23 +0100

    1.0.2

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactNavigationContext from './context';
 import withNavigation from './withNavigation';
 import { useNavigate } from "react-router-dom";
 
+let pathHistory = [];
 
 function NavigationProvider(props) {
 
@@ -13,24 +14,25 @@ function NavigationProvider(props) {
     }
 
     function _push(_link, _state) {
-
-        navigate(_link , _state);
+        pathHistory.push(_link);
+        navigate(_link, _state);
         window.history.pushState(_state, "state");
     }
 
     function _goBack() {
+        pathHistory.pop();
         navigate(-1);
     }
 
-    function _getPath(){
-        return window.history?.location?.pathname;
+    function _getPath() {
+        return window.history?.location?.pathname || ((pathHistory.length > 0) ? pathHistory.slice(-1) : "/");
     }
 
     return <ReactNavigationContext.Provider value={{
         navigation: {
             push: _push.bind(this),
             goBack: _goBack.bind(this),
-            getPath : _getPath.bind(this),
+            getPath: _getPath.bind(this),
         }
     }}>
         {props.children}
