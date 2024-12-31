f6602eadb07e084e5475e820683a256eb5e33582
Rémy <remy.arnaudin@sauermanngroup.com>
2024-12-31 08:26:05 +0100

    2.0.1

--- src/index.js
+++ src/index.js
@@ -7,6 +7,21 @@ function NavigationProvider(props) {
 
     const navigate = useNavigate();
 
+    function _push(_link, _state) {
+        pathHistory.push(_link);
+        navigate(_link, _state);
+        window.history.pushState(_state, "state");
+    }
+
+    function _goBack() {
+        pathHistory.pop();
+        navigate(-1);
+    }
+
+    function _getPath() {
+        return window.history?.location?.pathname || ((pathHistory.length > 0) ? pathHistory.slice(-1) : "/");
+    }
+    
     return <NavigationContext.Provider value={{
         push: _push.bind(this),
         goBack: _goBack.bind(this),
