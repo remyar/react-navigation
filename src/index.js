4e8bb1867f007d57e2556e041dd91af15694fa65
Rémy <remy.arnaudin@sauermanngroup.com>
2024-12-31 16:14:59 +0100

    sdfsdf

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext } from 'react';
+import React, { createContext, useContext, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 
 const NavigationContext = createContext();
