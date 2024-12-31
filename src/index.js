875234a6b458b427af2c828d2094218d8d301958
Rémy <remy.arnaudin@sauermanngroup.com>
2024-12-31 08:21:52 +0100

    2.0.0

--- src/index.js
+++ src/index.js
@@ -1,42 +1,23 @@
-import React from 'react';
-import ReactNavigationContext from './context';
-import withNavigation from './withNavigation';
+import React, { createContext, useContext } from 'react';
 import { useNavigate } from "react-router-dom";
 
-let pathHistory = [];
+const NavigationContext = createContext();
 
 function NavigationProvider(props) {
 
-    let navigate = useNavigate();
+    const navigate = useNavigate();
 
-    window.onpopstate = () => {
-        _goBack()
-    }
-
-    function _push(_link, _state) {
-        pathHistory.push(_link);
-        navigate(_link, _state);
-        window.history.pushState(_state, "state");
-    }
-
-    function _goBack() {
-        pathHistory.pop();
-        navigate(-1);
-    }
-
-    function _getPath() {
-        return window.history?.location?.pathname || ((pathHistory.length > 0) ? pathHistory.slice(-1) : "/");
-    }
-
-    return <ReactNavigationContext.Provider value={{
-        navigation: {
-            push: _push.bind(this),
-            goBack: _goBack.bind(this),
-            getPath: _getPath.bind(this),
-        }
-    }}>
+    return <NavigationContext.Provider value={{
+        push: _push.bind(this),
+        goBack: _goBack.bind(this),
+        getPath: _getPath.bind(this),
+    }} >
         {props.children}
-    </ReactNavigationContext.Provider >;
+    </NavigationContext.Provider>;
+};
+
+function useNavigation() {
+    return useContext(NavigationContext)
 }
 
-export { NavigationProvider, withNavigation }
+export { NavigationProvider, useNavigation };
